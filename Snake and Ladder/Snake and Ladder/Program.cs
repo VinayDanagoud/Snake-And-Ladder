2f81188489498026e870053778f26a3f328ab4c9
VinayDanagoud <vinaydanagoud143@gmail.com>
2022-04-04 15:44:31 +0530

    Player winning Position is 100

--- Snake and Ladder/Snake and Ladder/Program.cs
+++ Snake and Ladder/Snake and Ladder/Program.cs
@@ -1,30 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome TO Snake And Ladder");
+Console.WriteLine("Welcome To Snake and Ladder game");
 
-
-const int Start = 0;
-const int Finish = 100;
-const int Noplay = 0,
-          Ladder = 1,
-          Snake = 2;
+const int START = 0;
+int FINISH = 100;
+const int NO_PLAY = 0,
+          LADDER = 1,
+          SNAKE = 2;
 Random random = new Random();
 
 int Roll_Dice()
+
 {
     // random value is given to palyer's dice
     return random.Next(1, 7);
 }
+
 int Player_Move_Option(int player_Roll_Dice)
+
 {
     // Player Option is gentrated..
     int check_Player_Option = random.Next(0, 3);
     int player_Move = 0;
     switch (check_Player_Option)
     {
-        case Ladder:
+        case LADDER:
             player_Move = player_Roll_Dice;
             break;
-        case Snake:
+        case SNAKE:
             player_Move = -player_Roll_Dice;
             break;
         default:
@@ -39,22 +41,24 @@ int Player_Move_Option(int player_Roll_Dice)
     int player_position = 0, player_Next_Position;
 
     //Repeats till the Player reaches the winning position 100.
-    while (player_position < Finish)
+    while (player_position < FINISH)
     {
         // player rolls the dice and gets the value
         int player_Roll_Dice = Roll_Dice();
 
         // The Player then checks for a Option. They are No_Play,Ladder and Snake.
         int player_move = Player_Move_Option(player_Roll_Dice);
-        player_Next_Position = player_position + player_move;
+        if (player_position + player_move > FINISH)
+            player_Next_Position = player_position;
+        else
+            player_Next_Position = player_position + player_move;
 
-        if (player_Next_Position < Start)
-            player_position = Start;
+        if (player_Next_Position < START)
+            player_position = START;
         else
             player_position = player_Next_Position;
     }
     Console.WriteLine("Player current position After the game is " + player_position);
 }
-
 
 
